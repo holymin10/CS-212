de26b248395d784a1f6a45e30dd681947a9b9559
Sungmin Choi <99283340+holymin10@users.noreply.github.com>
2022-10-08 08:30:06 +0900

    Add files via upload

---
+++ project 1/project 1/Program.cs
@@ -0,0 +1,45 @@
+﻿/*********************************************************************
+* Simple sample C# selection, showing stdin, stdout, static, cetera
+*
+* Sungmin Choi 
+* CS212B
+* 9/13/22
+*
+*********************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a Program to compute floor lg lg n! ");
+            while (true)
+            {
+                Console.Write("\nEnter wanted input (n): ");
+                double n = double.Parse(Console.ReadLine());
+                double lgn = Lg(n);
+                double lglgn = Lg(Lg(n));
+
+                if (n <= 1)
+                    Console.Write("Invalid input, please input a value larger than 1");
+                else
+                    Console.WriteLine("lg(lg(({0})) = {1}.", n, lglgn);
+            }
+        }
+        static double Lg(double number)
+        {
+            double result = 0;
+            while (number > 1)
+            {
+                number /= 2;
+                result++;
+            }
+            return result;
+        }
+    }
+}
